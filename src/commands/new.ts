919e7a002f27f4e5e81c3f665fa70e7d7a66a478
Daniel Lopes <daniel.backup39@gmail.com>
2021-11-26 21:35:35 -0300

    Removing default git repository

--- src/commands/new.ts
+++ src/commands/new.ts
@@ -2,25 +2,29 @@ import { GluegunToolbox } from 'gluegun'
 import * as shell from 'shelljs'
 
 module.exports = {
-    name: 'new',
-    alias: ['n'],
-    run: async (toolbox: GluegunToolbox) => {
-        const { print, parameters } = toolbox;
-        if (!shell.which('git')) {
-            print.error('It is necessary to have git installed');
-            return;
-        }
-        if (!shell.which('node') || !shell.which('npm')) {
-            print.error('It is necessary to have node installed');
-            return;
-        }
-        shell.exec(`git clone https://github.com/technolopesdev/oh-mynode.git ${parameters.options.dir || 'oh-mynode'}`);
-        shell.cd(parameters.options.dir || 'oh-mynode')
-        print.info('Install dependencies...');
-        if (shell.which('yarn')) {
-            shell.exec('yarn install')
-            return;
-        }
-        shell.exec('npm install')
+  name: 'new',
+  alias: ['n'],
+  run: async (toolbox: GluegunToolbox) => {
+    const { print, parameters } = toolbox
+    if (!shell.which('git')) {
+      print.error('It is necessary to have git installed')
+      return
     }
+    if (!shell.which('node') || !shell.which('npm')) {
+      print.error('It is necessary to have node installed')
+      return
+    }
+    shell.exec(
+      `git clone https://github.com/technolopesdev/oh-mynode.git ${parameters
+        .options.dir || 'oh-mynode'}`
+    )
+    shell.cd(parameters.options.dir || 'oh-mynode')
+    shell.rm('.git');
+    print.info('Install dependencies...')
+    if (shell.which('yarn')) {
+      shell.exec('yarn install')
+      return
+    }
+    shell.exec('npm install')
+  }
 }
